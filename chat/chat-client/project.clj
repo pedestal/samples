6ae61bc5a7068d63d482f550c5babbb6de20d223
Tim Ewald <tim.ewald@gmail.com>
2013-03-15 17:26:40 -0400

    Push version number to 0.1.0

--- chat/chat-client/project.clj
+++ chat/chat-client/project.clj
@@ -1,12 +1,12 @@
-(defproject chat-client "0.0.1-SNAPSHOT"
+(defproject chat-client "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.namespace "0.2.1"]
                  [domina "1.0.1"]
                  [ch.qos.logback/logback-classic "1.0.6"]
                  [org.clojure/clojurescript "0.0-1450"]
-                 [io.pedestal/pedestal.app "0.0.9-SNAPSHOT"]
-                 [io.pedestal/pedestal.app-tools "0.0.9-SNAPSHOT"]]
+                 [io.pedestal/pedestal.app "0.1.0-SNAPSHOT"]
+                 [io.pedestal/pedestal.app-tools "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:source-paths ["dev"]}}
   :source-paths ["app/src" "app/templates"]
   :resource-paths ["config"]
