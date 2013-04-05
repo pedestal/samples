601ca5e219ed0c13d78781e081db00423f6a0283
Relevance Pair <pair@thinkrelevance.com>
2013-04-05 13:43:54 -0400

    Merge

--- cors/project.clj
+++ cors/project.clj
@@ -15,8 +15,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [io.pedestal/pedestal.service "0.1.2-SNAPSHOT"]
-                 [io.pedestal/pedestal.jetty "0.1.2"]
+                 [io.pedestal/pedestal.service "0.1.3"]
+                 [io.pedestal/pedestal.jetty "0.1.3"]
+                 [ch.qos.logback/logback-classic "1.0.7"]
                  [org.slf4j/jul-to-slf4j "1.7.2"]
                  [org.slf4j/jcl-over-slf4j "1.7.2"]
                  [org.slf4j/log4j-over-slf4j "1.7.2"]
