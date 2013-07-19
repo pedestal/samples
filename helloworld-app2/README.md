c0ac0102a0e98d35b097f27f93935d34d6df2560
Gabriel Horner <gabriel.horner@gmail.com>
2013-07-18 20:40:19 -0400

    generated new helloworld app

---
+++ helloworld-app2/README.md
@@ -0,0 +1,9 @@
+# helloworld-app2
+
+Start working on this application by writing its behavior in the file
+`app/src/helloworld_app2/behavior.clj`
+
+## Links
+
+* [Getting Started](https://github.com/pedestal/pedestal/tree/master/app#usage)
+* [Overview of how pedestal applications work](http://pedestal.io/documentation/application-overview/)
---
+++ helloworld-app2/app/src/helloworld_app2/start.cljs
@@ -0,0 +1,19 @@
+(ns helloworld-app2.start
+  (:require [io.pedestal.app.protocols :as p]
+            [io.pedestal.app :as app]
+            [io.pedestal.app.render.push :as push-render]
+            [io.pedestal.app.render :as render]
+            [io.pedestal.app.messages :as msg]
+            [helloworld-app2.behavior :as behavior]
+            [helloworld-app2.rendering :as rendering]))
+
+(defn create-app [render-config]
+  (let [app (app/build behavior/example-app)
+        render-fn (push-render/renderer "content" render-config render/log-fn)
+        app-model (render/consume-app-model app render-fn)]
+    (app/begin app)
+    (p/put-message (:input app) {msg/type :set-value msg/topic [:greeting] :value "Hello World!"})
+    {:app app :app-model app-model}))
+
+(defn ^:export main []
+  (create-app (rendering/render-config)))
