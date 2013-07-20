c0d58a659551f873e4caceefda78a4a97ca96589
Gabriel Horner <gabriel.horner@gmail.com>
2013-07-20 10:40:59 -0400

    add send-message transform and effect

--- chat/new-client/app/src/chat_client/behavior.clj
+++ chat/new-client/app/src/chat_client/behavior.clj
@@ -2,12 +2,31 @@
     (:require [clojure.string :as string]
               [io.pedestal.app :as app]
               [io.pedestal.app.dataflow :as d]
-              [io.pedestal.app.messages :as msg]))
+              [io.pedestal.app.util.platform :as platform]
+              [io.pedestal.app.messages :as msg]
+              [chat-client.util :as util]))
 
+;; Transforms
 (defn nickname-transform
   [_ message]
   (:nickname message))
 
+(defn send-message
+  [old-value message]
+  (let [msg {:id (util/random-id)
+             :time (platform/date)
+             :nickname (:nickname message)
+             :text (:text message)
+             :status :pending}]
+    (-> old-value
+        (update-in [:sent] conj msg)
+        (assoc :sending msg))))
+
+;; Effect
+(defn send-message-to-server [outbound]
+  [{msg/topic :server :out-message (:sending outbound)}])
+
+;; Emits
 (defn init-app-model [_]
   [{:chat
     {:log {}
@@ -52,9 +71,11 @@
   {:version 2
    :transform [
                [:set-nickname [:nickname] nickname-transform]
-               [:clear-nickname [:nickname] (constantly nil)]]
+               [:clear-nickname [:nickname] (constantly nil)]
+               [:send-message [:outbound] send-message]]
+   :effect #{[#{[:outbound]} send-message-to-server :single-val]}
    :emit [{:init init-app-model}
           [#{[:nickname]} chat-emit]
-          [#{[:*]} (app/default-emitter [])]
+          ;[#{[:*]} (app/default-emitter [])]
           ]})
 
--- chat/new-client/test/chat_client/test/behavior.clj
+++ chat/new-client/test/chat_client/test/behavior.clj
@@ -2,9 +2,11 @@
   (:require [io.pedestal.app :as app]
             [io.pedestal.app.protocols :as p]
             [io.pedestal.app.tree :as tree]
+            [io.pedestal.app.util.platform :as platform]
             [io.pedestal.app.messages :as msg]
             [io.pedestal.app.render :as render]
-            [io.pedestal.app.util.test :as test])
+            [io.pedestal.app.util.test :as test]
+            [chat-client.util :as util])
   (:use clojure.test
         chat-client.behavior
         [io.pedestal.app.query :only [q]]))
@@ -28,22 +30,38 @@
            (-> app :state deref :io.pedestal.app/emitter-deltas))
         "Emits correct deltas")
     (is (= (:value options)
-           (-> app :state deref :data-model node))
-        "Modifies correct data model value")))
+           (-> app :state deref :data-model (get-in node)))
+        "Modifies correct data model value")
+    (when (:with-state options)
+      ((:with-state options) (-> app :state deref)))))
 
 (deftest test-set-nickname
   (message-generates-deltas
    {msg/type :set-nickname msg/topic [:nickname] :nickname "Mick"}
    (set-nickname-deltas "Mick")
-   :node :nickname
+   :node [:nickname]
    :value "Mick"))
 
 (deftest test-clear-nickname
   (message-generates-deltas
    {msg/type :clear-nickname msg/topic [:nickname]}
    clear-nickname-deltas
-   :node :nickname
+   :node [:nickname]
    :value nil))
+
+(deftest test-send-message
+  (with-redefs [platform/date (constantly :date)
+                util/random-id (constantly 42)]
+    (let [msg {:id 42 :time :date :nickname "RR" :text "We have touchdown" :status :pending}]
+      (message-generates-deltas
+        {msg/type :send-message msg/topic [:outbound] :text "We have touchdown" :nickname "RR"}
+        []
+        :node [:outbound :sent]
+        :value (list msg)
+        :with-state (fn [state]
+                      (is (= [{msg/topic :server :out-message msg}]
+                             (:effect state))
+                          "Sends :server effect message"))))))
 
 ;; Use io.pedestal.app.query to query the current application model
 
