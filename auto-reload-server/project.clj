f1dad5ff9bbe65a03307d5418ca073f96ea15a6c
Rob Jentzema <rob.jentzema@googlemail.com>
2013-09-01 21:44:30 +0200

    updated samples dependencies to latest stable pre 0.2.0 (0.1.10)

--- auto-reload-server/project.clj
+++ auto-reload-server/project.clj
@@ -4,21 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [io.pedestal/pedestal.service "0.1.9-SNAPSHOT"]
+                 [io.pedestal/pedestal.service "0.1.10"
+                   :exclusions [org.slf4j/slf4j-api]]
 
                  ;; Remove this line and uncomment the next line to
                  ;; use Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.1.9-SNAPSHOT"]
-                 ;; [io.pedestal/pedestal.tomcat "0.1.9-SNAPSHOT"]
+                 [io.pedestal/pedestal.jetty "0.1.10"]
+                 ;; [io.pedestal/pedestal.tomcat "0.1.10"]
 
                  ;; auto-reload changes
                  [ns-tracker "0.2.1"]
 
                  ;; Logging
-                 [ch.qos.logback/logback-classic "1.0.7" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.2"]
-                 [org.slf4j/jcl-over-slf4j "1.7.2"]
-                 [org.slf4j/log4j-over-slf4j "1.7.2"]]
+                 [ch.qos.logback/logback-classic "1.0.13"]
+                 [org.slf4j/jul-to-slf4j "1.7.5"]
+                 [org.slf4j/jcl-over-slf4j "1.7.5"]
+                 [org.slf4j/log4j-over-slf4j "1.7.5"]]
   :profiles {:dev {:source-paths ["dev"]}}
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
--- helloworld-app/project.clj
+++ helloworld-app/project.clj
@@ -12,9 +12,10 @@
 (defproject helloworld-app "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1586"]
+                 [org.clojure/clojurescript "0.0-1859"]
                  [domina "1.0.1"]
-                 [ch.qos.logback/logback-classic "1.0.7" :exclusions [org.slf4j/slf4j-api]]
+                 [ch.qos.logback/logback-classic "1.0.13"
+                  :exclusions [org.slf4j/slf4j-api]]
                  [io.pedestal/pedestal.app "0.1.10"]
                  [io.pedestal/pedestal.app-tools "0.1.10"]]
   :profiles {:dev {:source-paths ["dev"]}}
